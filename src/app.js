38e4e2aed51d6182c655a38f75dad7184343cc05
fabeto10 <f0414475@gmail.com>
2022-05-06 14:39:38 +0000

    Generador de excusas completado proyecto JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = excuseGenerator();
+  });
+};
+
+let excuseGenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIdex = Math.floor(Math.random() * who.length);
+  let actionIdex = Math.floor(Math.random() * action.length);
+  let whatIdex = Math.floor(Math.random() * what.length);
+  let whenIdex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIdex] +
+    " " +
+    action[actionIdex] +
+    " " +
+    what[whatIdex] +
+    " " +
+    when[whenIdex]
+  );
 };
